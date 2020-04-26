65099d7713e6c5e2025bc81c59284232835aa1d5
Md. Anisur Rahman <anis112@gmail.com>
2020-04-26 15:16:49 +0600

    Model 'LookUpCcModCertificateFormat' updated

--- WrpCcNocWeb/Models/CcModule/LookUpCcModCertificateFormat.cs
+++ WrpCcNocWeb/Models/CcModule/LookUpCcModCertificateFormat.cs
@@ -14,25 +14,42 @@ namespace WrpCcNocWeb.Models.CcModule
         public int CertificateFormatId { get; set; }
 
         [Required]
-        [Column("RuleAsPerGazette", Order = 1)]
+        [Column("ProjectTypeId", Order = 1)]
+        [Display(Name = "Project Type")]
+        public int ProjectTypeId { get; set; }
+        [ForeignKey("ProjectTypeId")]
+        public virtual LookUpCcModProjectType LookUpCcModProjectType { get; set; }
+
+        [Required]
+        [Column("RuleAsPerGazette", Order = 2)]
         [MaxLength(100)]
         [Display(Name = "Rule As Per Gazette")]
         public string RuleAsPerGazette { get; set; }
 
-        [Column("ProjectTypeId", Order = 2)]
-        [Display(Name = "Project Type")]
-        public long? ProjectTypeId { get; set; }
-        [ForeignKey("ProjectTypeId")]
-        public virtual LookUpCcModProjectType LookUpCcModProjectType { get; set; }
-                
         [Required]
-        [Column("CertificateBody", Order = 3)]
-        [MaxLength(200)]        
+        [Column("RuleAsPerGazetteBn", Order = 3)]
+        [MaxLength(100)]
+        [Display(Name = "Rule As Per Gazette")]
+        public string RuleAsPerGazetteBn { get; set; }
+
+        [Required]
+        [Column("CertificateBody", Order = 4)]
+        [MaxLength(500)]        
         public string CertificateBody { get; set; }
 
         [Required]
-        [Column("TermsConditions", Order = 4)]
-        [MaxLength(550)]
+        [Column("CertificateBodyBn", Order = 5)]
+        [MaxLength(500)]
+        public string CertificateBodyBn { get; set; }
+
+        [Required]
+        [Column("TermsConditions", Order = 6)]
+        [MaxLength(1000)]
         public string TermsConditions { get; set; }
+
+        [Required]
+        [Column("TermsConditionsBn", Order = 7)]
+        [MaxLength(1000)]
+        public string TermsConditionsBn { get; set; }
     }
 }
