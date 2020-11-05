8a18dad3cad19561022120c89332ef800f1a57c8
ATM Saidul Karim RonY <atmskrony@hotmail.com>
2020-11-05 11:47:04 +0600

    Registry book done.

--- WrpCcNocWeb/Controllers/reportController.cs
+++ WrpCcNocWeb/Controllers/reportController.cs
@@ -11,6 +11,7 @@ using WrpCcNocWeb.Models.Utility;
 using static WrpCcNocWeb.Helpers.CommonHelper;
 using Rotativa.AspNetCore;
 using Rotativa.AspNetCore.Options;
+using System.Globalization;
 
 namespace WrpCcNocWeb.Controllers
 {
@@ -46,6 +47,7 @@ namespace WrpCcNocWeb.Controllers
 
             UserLevelInfo uli = HttpContext.Session.GetComplexData<UserLevelInfo>("UserLevelInfo");
             List<CcRegistryBook> rbList = new List<CcRegistryBook>();
+            var cultureInfo = new CultureInfo("en-US");
 
             try
             {
@@ -66,7 +68,7 @@ namespace WrpCcNocWeb.Controllers
                           join upz in _db.LookUpAdminBndUpazila on loc.UpazilaGeoCode equals upz.UpazilaGeoCode into upzList
                           from upaz in upzList.DefaultIfEmpty()
 
-                          //where c.ApprovalStatusId == 1
+                          where c.ApprovalStatusId == 1
 
                           select new CcRegistryBook
                           {
@@ -77,8 +79,8 @@ namespace WrpCcNocWeb.Controllers
                               ProjectDistrict = dist.DistrictName,
                               ProjectUpazila = upaz.UpazilaName,
                               MethodOfUsing = "",
-                              IssuingDate = c.AppApprovalDate.Value.ToString("dd MMM, yyyy"),
-                              ExpiringDate = c.AppApprovalDate.Value.AddYears(2).ToString("dd MMM, yyyy"),
+                              IssuingDate = (c.AppApprovalDate != null) ? c.AppApprovalDate.Value.ToString("dd MMM, yyyy", cultureInfo) : string.Empty,
+                              ExpiringDate = (c.AppApprovalDate != null) ? c.AppApprovalDate.Value.AddYears(2).ToString("dd MMM, yyyy", cultureInfo) : string.Empty,
                               ClearanceTerms = ""
                           }).OrderBy(o => o.AppSubmissionId).ToList();
 
@@ -107,6 +109,83 @@ namespace WrpCcNocWeb.Controllers
             //};
 
             return View();
+        }
+
+        //report/regprint
+        public IActionResult regprint()
+        {
+            ViewData["Title"] = "Registry Book of Clearance Certificate";
+            UserInfo ui = HttpContext.Session.GetComplexData<UserInfo>("LoggerUserInfo");
+            if (ui == null)
+            {
+                return RedirectToAction("login", "account");
+            }
+
+            UserLevelInfo uli = HttpContext.Session.GetComplexData<UserLevelInfo>("UserLevelInfo");
+            List<CcRegistryBook> rbList = new List<CcRegistryBook>();
+            var cultureInfo = new CultureInfo("en-US");
+
+            try
+            {
+                rbList = (from c in _db.CcModAppProjectCommonDetail
+
+                          join proj in _db.LookUpCcModProjectType on c.ProjectTypeId equals proj.ProjectTypeId into projList
+                          from pt in projList.DefaultIfEmpty()
+
+                          join user in _db.AdminModUsersDetail on c.UserId equals user.UserId into userList
+                          from u in userList.DefaultIfEmpty()
+
+                          join l in _db.CcModPrjLocationDetail on c.ProjectId equals l.ProjectId into locList
+                          from loc in locList.DefaultIfEmpty()
+
+                          join dst in _db.LookUpAdminBndDistrict on loc.DistrictGeoCode equals dst.DistrictGeoCode into dstList
+                          from dist in dstList.DefaultIfEmpty()
+
+                          join upz in _db.LookUpAdminBndUpazila on loc.UpazilaGeoCode equals upz.UpazilaGeoCode into upzList
+                          from upaz in upzList.DefaultIfEmpty()
+
+                          where c.ApprovalStatusId == 1
+
+                          select new CcRegistryBook
+                          {
+                              AppSubmissionId = c.AppSubmissionId,
+                              ApplicantNameAddress = u.ApplicantTypeId == 1 ? String.Format("{0}<br />{1}<br />{2}", u.ApplicantName, u.UserProfession, u.PostalAddress) : String.Format("{0}<br />{1}<br />{2}", u.OrganizationName, u.UserDesignation, u.PostalAddress),
+                              ProjectType = pt.ProjectType,
+                              ProjectObjective = c.ProjectObjective,
+                              ProjectDistrict = dist.DistrictName,
+                              ProjectUpazila = upaz.UpazilaName,
+                              MethodOfUsing = "",
+                              IssuingDate = (c.AppApprovalDate != null) ? c.AppApprovalDate.Value.ToString("dd MMM, yyyy", cultureInfo) : string.Empty,
+                              ExpiringDate = (c.AppApprovalDate != null) ? c.AppApprovalDate.Value.AddYears(2).ToString("dd MMM, yyyy", cultureInfo) : string.Empty,
+                              ClearanceTerms = ""
+                          }).OrderBy(o => o.AppSubmissionId).ToList();
+
+                if (rbList.Count > 0)
+                {
+                    ViewData["RegistryBookList"] = rbList;
+                }
+                else
+                {
+                    ViewData["RegistryBookList"] = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                rbList = new List<CcRegistryBook>();
+                string message = ch.ExtractInnerException(ex);
+                TempData["Message"] = ch.ShowMessage(Sign.Error, "An Error Occured", message);
+                ViewData["RegistryBookList"] = null;
+            }
+
+            return new ViewAsPdf("~/Views/report/regprint.cshtml", viewData: ViewData)
+            {
+                PageSize = Rotativa.AspNetCore.Options.Size.A4,
+                PageOrientation = Orientation.Landscape,
+                PageMargins = new Margins(10, 10, 10, 10),
+                CustomSwitches = "--page-offset 0 --footer-center [page] --footer-font-size 8"
+            };
+
+            //return View();
         }
     }
 }
